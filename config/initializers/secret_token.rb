245b2c814ba87b7d3db7593070bfc489140b0fb7
Aaron Mills <aaron@jailbreakcreative.com>
2011-02-16 20:49:07 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CryOutAmerica::Application.config.secret_token = '58f2574274788005dc2196f590daf3426647303f649771ce4aee10afd8eb20f8a79b6b1eb53d31e3e78a6cc8dd055fb0fe0ec6ea4b10510de31621128b8d405f'
